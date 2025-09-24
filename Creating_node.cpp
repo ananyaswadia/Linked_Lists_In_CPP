345410c2f3642da549d00cb08b1976a7a2e46b35
Ananya Swadia <ananya.swadia@gmail.com>
2025-09-24 01:48:17 -0400

    Create Creating_node.cpp

---
+++ Creating_node.cpp
@@ -0,0 +1,26 @@
+//Ananya Swadia
+//@4070123012
+
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node*next;
+    
+    // constructor to initialise
+    
+    Node(int data){
+        val = data;
+        next = NULL;
+    }
+};
+int main (){
+    Node*n = new Node(10);
+    cout<<<<n->val<<" "<<n->next<<endl;
+    return 0;
+}
+
+/* output 
+10 0 
+*/
